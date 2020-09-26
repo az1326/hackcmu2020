b344f32c13de59379e8ebe00c6a5c4abd3c515a2
sreehirani <71532514+sreehirani@users.noreply.github.com>
2020-09-25 21:57:07 -0400

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,1 +1,29 @@
-print("hello")
+import math
+
+symbols_dict = {
+    "less than or equal to": "\leq",
+    "geater than or equal to": "\geq",
+    "not equal to": "\\neq",
+    "times": "\\times",
+    "integer divided by": "\div",
+    "plus or minus": "\pm",
+    "infinity": "\infty",
+    "pie": "\pi",
+    "theta": "\\theta",
+    "approximately": "\\approx",
+    "two": "2"
+    
+}
+
+def mathSymbolsGeneral(math_array):
+    result = ""
+    
+    if string == "":       
+    return result
+
+def poly_str(str):
+    return polynomial(str.split())
+
+
+print(parse("V times W")) 
+print(poly_str("2 x to the fourth plus x squared minus x"))
