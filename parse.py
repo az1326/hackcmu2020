7c03af4a87becd7a56b6ae76d076f152d15d2f25
Jeremy <jchiu342@gmail.com>
2020-09-26 12:26:27 -0400

    multiple uses for a word

--- parse.py
+++ parse.py
@@ -5,6 +5,7 @@
 
 import re
 
+
 dict = {
     "zero": "0",
     "one": "1",
@@ -28,14 +29,16 @@ dict = {
     "squared": "^2",
     "cube": "^3",
     "cubed": "^3",
-    "plus": "+",
-    "minus": "-",
-    "second": "2",
-    "third": "3",
-    "fourth": "4",
-    "fifth": "5",
-    "root": "{\\sqrt",
-    "there": ["exists", "\\exists "]
+    "plus": ["+"],
+    "minus": ["-"],
+    "second": ["2"],
+    "third": ["3"],
+    "fourth": ["4"],
+    "fifth": ["5"],
+    "root": ["{\\sqrt"],
+    "there": ["exists", "\\exists "],
+    "left": [["bracket", "{"], ["parenthesis", "("]],
+    "right": [["bracket", "}"], ["parenthesis", ")"]]
 }
 
 
@@ -44,42 +47,53 @@ def process(word_array, start_index, end_index):
     result = ""
     i = start_index
     while i < end_index:
-        raw_str = dict.get(word_array[i], word_array[i])
-        if isinstance(raw_str, list):
-            flag = True
-            for j in range(0, len(raw_str) - 1):
-                if i + j + 1 >= end_index or word_array[i + j + 1] != raw_str[j]:
-                    flag = False
+        raw_arr = dict.get(word_array[i], word_array[i])
+        word_flag = False
+        for raw_str in raw_arr:
+            if isinstance(raw_str, list):
+                flag = True
+                for j in range(0, len(raw_str) - 1):
+                    if i + j + 1 >= end_index or word_array[i + j + 1] != raw_str[j]:
+                        flag = False
+                        break
+                if flag:
+                    result += raw_str[len(raw_str) - 1]
+                    print(result)
+                    i += len(raw_str) - 1
+                    word_flag = True
                     break
-            if flag:
-                result += raw_str[len(raw_str) - 1]
-                i += len(raw_str) - 1
+            elif raw_str == "fraction":
+                j = i + 1
+                while j < len(word_array) and word_array[j] != "fraction":
+                    j += 1
+                result += fraction(word_array, i, j)
+                i = j
+                word_flag = True
+                break
+            elif raw_str == "matrix":
+                j = i + 1
+                while j < len(word_array) and word_array[j] != "matrix":
+                    j += 1
+                result += matrix(word_array, i+1, j)
+                i = j
+                word_flag = True
+                break
+            elif raw_str[0] == '{':
+                j = i + 1
+                while j < len(word_array) and dict.get(word_array[j]) != raw_str:
+                    j += 1
+                result += single_brace_function(raw_str[1:], word_array, i, j)
+                i = j
+                word_flag = True
+                break
             else:
-                result += word_array[i]
-        elif raw_str == "to" and i + 1 < len(word_array) and word_array[i + 1] == "the":
-            result += "^"
-            i += 1
-        elif raw_str == "fraction":
-            j = i + 1
-            while j < len(word_array) and word_array[j] != "fraction":
-                j += 1
-            result += fraction(word_array, i, j)
-            i = j
-        elif raw_str == "matrix":
-            j = i + 1
-            while j < len(word_array) and word_array[j] != "matrix":
-                j += 1
-            result += matrix(word_array, i+1, j)
-            i = j
-        elif raw_str[0] == '{':
-            j = i + 1
-            while j < len(word_array) and dict.get(word_array[j]) != raw_str:
-                j += 1
-            result += single_brace_function(raw_str[1:], word_array, i, j)
-            i = j
-        else:
-            result += raw_str
-            if len(raw_str) > 2:
+                result += raw_str
+                if len(raw_str) > 2:
+                    result += ' '
+                word_flag = True
+        if not word_flag:
+            result += word_array[i]
+            if len(word_array[i]) > 2:
                 result += ' '
         i += 1
     return result
@@ -164,4 +178,4 @@ def matrix(word_array, start_index, end_index):
     return result
         
 
-print(poly_str("there exists vector v 2 x plus four power second"))
+print(poly_str("left parenthesis left bracket second right parenthesis"))
