e7c5799dc8e624102295713a04f058b6d4d0846c
Alan Zhu <alanzhu2002@gmail.com>
2020-09-26 15:51:37 -0400

    Commented out test cases

--- latex_rec.py
+++ latex_rec.py
@@ -15,6 +15,7 @@ with sr.AudioFile(AUDIO_FILE) as source:
 # with sr.Microphone() as source:
 #     print("Say something!")
 #     audio = r.listen(source)
+# print("Working...")
 
 try:
     # for testing purposes, we're just using the default API key
@@ -22,20 +23,19 @@ try:
     # instead of `r.recognize_google(audio)`
     recognized = r.recognize_google(audio)
     print("Google Speech Recognition thinks you said " + recognized)
-    recognized = "matrix " + recognized + " matrix"
-    rec_arr = re.findall(r"[\w']+", recognized)
+    rec_arr = recognized.split()
     print("This becomes\n" + process(rec_arr, 0 ,len(rec_arr)))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-s_test = """less than or equal to less than divided by divided"""
-sarr = s_test.split()
-print(process(sarr, 0, len(sarr)))
+# s_test = """less than or equal to less than divided by divided"""
+# sarr = s_test.split()
+# print(process(sarr, 0, len(sarr)))
 
-m_test = """one plus two squared minus root four close times fraction one two denominator three fraction
-            matrix element 1 plus 2 element 2 root 4 close element fraction 12 denominator 3 fraction row
-            element 2 element 4 element 7 matrix"""
-marr = m_test.split()
-print(process(marr, 0, len(marr)))
+# m_test = """one plus two squared minus root four close times fraction one two denominator three fraction
+#             matrix element 1 plus 2 element 2 root 4 close element fraction 12 denominator 3 fraction row
+#             element 2 element 4 element 7 matrix"""
+# marr = m_test.split()
+# print(process(marr, 0, len(marr)))
